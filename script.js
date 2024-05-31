054d762adb184aaee0827cb10004d3f913b6ccfc
Chaitanya Prasad <155820405+hrcprasad@users.noreply.github.com>
2024-05-31 11:36:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoiaHJjcHJhc2FkIiwiYSI6ImNsd3VhM2o2ODA0Y2UybXNhdmNnb3U2bGsifQ.0eRT_DsiN3-iJ8y8nojMDA';
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
